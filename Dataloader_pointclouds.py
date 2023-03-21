292b2c9046aefc5d5c290c60a4d8b0404b72ed4f
KarolineKlan <99348322+KarolineKlan@users.noreply.github.com>
2023-03-21 22:46:48 +0100

    Add files via upload

---
+++ Dataloader_pointclouds.py
@@ -0,0 +1,58 @@
+
+
+import open3d as o3d
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import glob
+
+
+class PointClouds(Dataset):
+    def __init__(self):
+
+        males = []
+        females =[]
+
+        #load females
+        for obj in glob.iglob("./data/female/*.obj"):
+            mesh = o3d.io.read_triangle_mesh(obj)
+
+            #cetralises each mesh object
+            vertices = np.asarray(mesh.vertices)
+            center = vertices.mean(axis=0)
+            vertices -= center
+            mesh.vertices = o3d.utility.Vector3dVector(vertices)
+
+            #converts mesh to pcd and downsamples to 2048 points
+            pcd = mesh.sample_points_uniformly(2048)
+            females.append(pcd)
+        
+        #load males
+        for obj in glob.iglob("./data/male/*.obj"):
+            mesh = o3d.io.read_triangle_mesh(obj)
+
+            #cetralises each mesh object
+            vertices = np.asarray(mesh.vertices)
+            center = vertices.mean(axis=0)
+            vertices -= center
+            mesh.vertices = o3d.utility.Vector3dVector(vertices)
+
+            #converts mesh to pcd and downsamples to 2048 points
+            pcd = mesh.sample_points_uniformly(2048)
+            males.append(pcd)
+
+        
+        self.males = males
+        self.females = females
+        self.pcds = np.concatenate((females, males), axis=0)
+    
+    def __len__(self):
+        return len(self.pcds)
+    
+    def __getitem__(self, idx):
+        sample = {'pointcloud': self.pcds[index]}
+        return sample
