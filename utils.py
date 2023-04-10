2ab128fe8613e52049d8eb0f8cadb18c2f677be7
JaSeKii <jacobsk2000@gmail.com>
2023-04-10 14:56:33 +0200

    added foldingNet as generator

---
+++ utils.py
@@ -0,0 +1,73 @@
+import random, torch, os, numpy as np
+import torch.nn as nn
+import config
+import copy
+import torch.nn.functional as F
+
+def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
+    print("=> Saving checkpoint")
+    checkpoint = {
+        "state_dict": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+    }
+    torch.save(checkpoint, filename)
+
+
+def load_checkpoint(checkpoint_file, model, optimizer, lr):
+    print("=> Loading checkpoint")
+    checkpoint = torch.load(checkpoint_file, map_location=config.DEVICE)
+    model.load_state_dict(checkpoint["state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+
+    # If we don't do this then it will just have learning rate of old checkpoint
+    # and it will lead to many hours of debugging \:
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = lr
+
+
+class ChamferLoss(nn.Module):
+    def __init__(self):
+        super(ChamferLoss, self).__init__()
+        self.use_cuda = torch.cuda.is_available()
+
+    def batch_pairwise_dist(self, x, y):
+        bs, num_points_x, points_dim = x.size()
+        _, num_points_y, _ = y.size()
+        xx = x.pow(2).sum(dim=-1)
+        yy = y.pow(2).sum(dim=-1)
+        zz = torch.bmm(x, y.transpose(2, 1))
+        rx = xx.unsqueeze(1).expand_as(zz.transpose(2, 1))
+        ry = yy.unsqueeze(1).expand_as(zz)
+        P = (rx.transpose(2, 1) + ry - 2 * zz)
+        return P
+
+    def forward(self, preds, gts):
+        P = self.batch_pairwise_dist(gts, preds)
+        mins, _ = torch.min(P, 1)
+        loss_1 = torch.sum(mins)
+        mins, _ = torch.min(P, 2)
+        loss_2 = torch.sum(mins)
+        return loss_1 + loss_2
+
+
+class CrossEntropyLoss(nn.Module):
+    def __init__(self, smoothing=True):
+        super(CrossEntropyLoss, self).__init__()
+        self.smoothing = smoothing
+    
+    def forward(self, preds, gts):
+        gts = gts.contiguous().view(-1)
+
+        if self.smoothing:
+            eps = 0.2
+            n_class = preds.size(1)
+
+            one_hot = torch.zeros_like(preds).scatter(1, gts.view(-1, 1), 1)
+            one_hot = one_hot * (1 - eps) + (1 - one_hot) * eps / (n_class - 1)
+            log_prb = F.log_softmax(preds, dim=1)
+
+            loss = -(one_hot * log_prb).sum(dim=1).mean()
+        else:
+            loss = F.cross_entropy(preds, gts, reduction='mean')
+
+        return loss
