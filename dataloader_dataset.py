3f95642987dc1e6337af4e191ac2dc78c25ae808
JaSeKii <97095356+JaSeKii@users.noreply.github.com>
2023-04-11 12:39:46 +0200

    update data

--- dataloader_dataset.py
+++ dataloader_dataset.py
@@ -12,7 +12,6 @@ import glob
 import os
 
 
-
 class PointCloudDataset(Dataset):
 
     def __init__(self, root_female="./data/female", root_male="./data/male", transform = None):
@@ -22,7 +21,10 @@ class PointCloudDataset(Dataset):
 
         self.object_female = os.listdir(root_female)
         self.object_male = os.listdir(root_male)
-        self.object_all = np.concatenate((self.object_female, self.object_male), axis=0)
+        self.length_dataset = max(len(self.object_female), len(self.object_male))
+        self.male_len = len(self.object_male)
+        self.female_len = len(self.object_female)
+        #self.object_all = np.concatenate((self.object_female, self.object_male), axis=0)
 
         self.furthest_distance = 1.103279724474332 #calculated in notebook 
         """"
@@ -36,34 +38,43 @@ class PointCloudDataset(Dataset):
         """
 
     def __len__(self):
-        return len(self.object_all)
+        return self.length_dataset
 
-    
+    #given index, output a pointcloud as a tensor from both domains, have been normalized
     def __getitem__(self, idx):
+        male_obj = [idx % self.male_len]
+        female_obj = [idx % self.female_len]
 
-        if idx < len(self.object_female):
-            obj_path = os.path.join(self.root_female, self.object_female[idx])
-            label = 0  # Female label
-        else: 
-            obj_path = os.path.join(self.root_male, self.object_male[idx - len(self.object_female)])
-            label = 1  # male label
-
-        #convert obj. into pointclouds
         mesh = o3d.io.read_triangle_mesh(obj_path)
         pointcloud = mesh.sample_points_uniformly(number_of_points=2048)
-
-        #normalize using furthest distance in whole dataset
-        centroid = np.mean(pointcloud.points, axis=0)
-        pointcloud.points -= centroid
-        pointcloud.points /= self.furthest_distance
-
-        if self.transform:
-            pointcloud = self.transform(pointcloud)
+        
+        male_pointcloud, female_pointcloud = 1,1
+        return male_pointcloud, female_pointcloud
 
 
-        #if np.random > 05. augmentate do be developed...
+    
+    # def __getitem__(self, idx):
 
-        return pointcloud, label
+    #     if idx < len(self.object_female):
+    #         obj_path = os.path.join(self.root_female, self.object_female[idx])
+    #         label = 0  # Female label
+    #     else: 
+    #         obj_path = os.path.join(self.root_male, self.object_male[idx - len(self.object_female)])
+    #         label = 1  # male label
+
+    #     #convert obj. into pointclouds
+    #     mesh = o3d.io.read_triangle_mesh(obj_path)
+    #     pointcloud = mesh.sample_points_uniformly(number_of_points=2048)
+
+    #     #normalize using furthest distance in whole dataset
+    #     centroid = np.mean(pointcloud.points, axis=0)
+    #     pointcloud.points -= centroid
+    #     pointcloud.points /= self.furthest_distance
+
+    #     if self.transform:
+    #         pointcloud = self.transform(pointcloud)
+    #     #if np.random > 05. augmentate to be developed...
+    #     return pointcloud, label
     
 
 data = PointCloudDataset()
